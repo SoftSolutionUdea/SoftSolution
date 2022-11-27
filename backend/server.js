0d964a1c83a260953623b1059c23cc6324edd936
Ingjaguar <ing.jaguarin@gmail.com>
2022-11-26 22:07:11 -0500

    update14

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cloudinary= require("cloudinary")
 //dotenv.config({path: 'backend/config/config.env'})
 
 //Seteamos archivo de configuracion
-if (process.env.NODE_ENV === "PRODUCTION") require('dotenv').config({path:'/backend/config/config.env'})
+if (process.env.NODE_ENV === "PRODUCTION") require('dotenv').config({path:'./backend/config/config.env'})
  
 //Configurar base de datos
 connectDatabase();
